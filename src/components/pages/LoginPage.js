b69fe542b220a341a3e3ce9bab817a00728786cd
samuelomondi22 <omondisam696@gmail.com>
2022-01-13 20:25:09 -0700

    first commit

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Text>Login Page</Text>
+        </div>
+    )
+}
+
+export default LoginPage;
